65c14cb78509ef0a2fe8068890141798b577154a
hsnachos <hsnachos@gmail.com>
2021-09-07 16:09:38 +0900

    프로젝트 생성

---
+++ src/main/java/kr/go/mapo/mpyouth/MpYouthApplication.java
@@ -0,0 +1,13 @@
+package kr.go.mapo.mpyouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpYouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpYouthApplication.class, args);
+	}
+
+}
